3ff3b3d5deccf3a7d3583937cdb5edb9c2f21fce
Harmanpreet Singh <harmanpreet.singh@iodatalabs.io>
2024-10-06 22:02:02 +0530

    Login page UI

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print(request.form)
+        # TODO: logic to process login feature
+        return redirect("/")
+    return render_template("login.html")
 
 @app.route("/")
 def index():
