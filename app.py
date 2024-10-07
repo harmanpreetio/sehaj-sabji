e1f7a13a2a0d2adf49f2e36fa7b3c1bc59b9085e
Harmanpreet Singh <harmanpreet.singh@iodatalabs.io>
2024-10-07 22:08:59 +0530

    ADD: Dashboard UI :zap:

--- app.py
+++ app.py
@@ -14,6 +14,24 @@ def login():
 def index():
     return render_template("index.html")
 
+@app.route("/dashboard")
+def dashboard():
+    # TODO: fetch sabji list from database
+    # convert to list
+    sabji_list = [
+        {"name": "Allo", "qty": "1kg"},
+        {"name": "Pyaaz", "qty": "2kg"},
+        {"name": "Tamatar", "qty": "1kg"},
+    ]
+    return render_template("dashboard.html", sabji_list=sabji_list)
+
+@app.route("/add-sabji", methods=["GET", "POST"])
+def add_sabji():
+    if request.method == "POST":
+        # TODO: logic to process sabji addition
+        print(request.form)
+        return redirect("/dashboard")
+    return render_template("add_sabji.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
